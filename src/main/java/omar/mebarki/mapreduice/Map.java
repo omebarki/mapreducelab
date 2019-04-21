96e71185a6dc82cec08d65d2f7df9cdc4b7158b7
Omar MEBARKI <omar.mebarki@gmail.com>
2019-04-21 21:07:00 +0200

    Use a Hashmap to reduce one line words

--- src/main/java/omar/mebarki/mapreduice/Map.java
+++ src/main/java/omar/mebarki/mapreduice/Map.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
@@ -13,8 +14,15 @@ public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
     @Override
     protected void map(LongWritable key, Text line, Context context) throws IOException, InterruptedException {
         StringTokenizer wordsTokenizer = new StringTokenizer(line.toString());
+        java.util.Map<String, Integer> wordsCount = new HashMap<String, Integer>();
         while (wordsTokenizer.hasMoreTokens()) {
-            context.write(new Text(wordsTokenizer.nextToken()), new IntWritable(1));
+            String word = wordsTokenizer.nextToken();
+            int initial = wordsCount.getOrDefault(word, 0);
+            wordsCount.put(word, ++initial);
+        }
+        for (java.util.Map.Entry<String, Integer> wordEntry :
+                wordsCount.entrySet()) {
+            context.write(new Text(wordEntry.getKey()), new IntWritable(wordEntry.getValue()));
         }
     }
 }
